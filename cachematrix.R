bd56e3f8507aad9c9cb62c9004442f5d7adfedda
grace-blip <grace.finlayson@rutgers.edu>
2023-11-21 11:38:53 +0100

    Edits to the cachematrix.R for submission

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##This function creates a list of functions to edit the matrix cache as well as the matrix inverse chache
 
+makeCacheMatrix <- function(x = matrix()) {
+  inverse<-NULL
+  setmatrix<-function(y){
+    inverse<<-NULL
+    x<<-y
+  }
+  getmatrix<-function() x
+  setinverse<- function(inv)inverse<<-inv
+  getinverse<- function() inverse
+  list(setmatrix=setmatrix, getmatrix=getmatrix, setinverse=setinverse, getinverse=getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
+##This function takes the matrix cache object and returns the matrix inverse, it first checks the cache and then calculates the inverse if necessary and saves it. 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse<-x$getinverse()
+  if(!is.null(inverse)){
+    return(inverse)
+  }
+  matrix<- x$getmatrix()
+  inverse<- solve(matrix)
+  x$setinverse(inverse)
+  inverse
 }
